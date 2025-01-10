0a14bc5eba65cfbe7ffdf0bc2ba7302c1eabab34
GabrielFRails <gabriel.estudy.reis@gmail.com>
2025-01-10 12:53:37 -0300

    Add endpoint to get cron job by id

--- libapi.py
+++ libapi.py
@@ -27,6 +27,38 @@ def api_crontab_get(user: str) -> tuple[bool, list[dict] | str]:
 	
 	return True, jobs
 
+def api_cronjob_get_by_id(user: str, id: str) -> tuple[bool, dict]:
+	success, crontab_content = run_crontab_command(user)
+	if not success:
+		return False, crontab_content  # crontab_content here is actually the error message
+	
+	job = None
+	success = False
+	cron_lines = crontab_content.split("\n")
+	for idx, line in enumerate(cron_lines):
+		line = line.strip()
+		if line and line.startswith("#cjid:"):
+			parts = line.split("%")
+			prefix = parts[0]
+			cjid = prefix.split(":")
+			
+			if cjid == id:
+				success, job = api_etl_crontab_line_todict(cron_lines[idx + 1])
+				job['cjid'] = cjid
+				return True, job
+	
+	return False, job
+
+def api_etl_crontab_line_todict(line):
+	parts = line.split(maxsplit=5)
+	if len(parts) < 6:
+		return False, f"Linha de crontab inválida: {line}"
+	schedule, command = ' '.join(parts[:5]), parts[5]
+	return True, {
+		"schedule": schedule,
+		"command": command
+	}
+
 def api_cronjob_add(cron_line: str) -> tuple[bool, str]:
     current_crontab = run(["crontab", "-l"], stdout=PIPE, stderr=PIPE, text=True)
 
--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def get_user_cron_jobs(user: str):
         raise HTTPException(status_code=500, detail=str(result))
     return result
 
+@app.get("/jobs/user/id")
+def get_cron_job(user: str, id: str):
+    success, result = api_cronjob_get_by_id(user, id)
+    if not success:
+        raise HTTPException(status_code=500, detail=str(result))
+    return result
+
 @app.post("/jobs/create-cron")
 async def create_cron_job(job: CronJob):
     cjid = str(uuid.uuid4()).split("-")[0]
