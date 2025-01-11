4c89b61927f6073d6b7802da18f8ec7cdced29fb
GabrielFRails <gabriel.estudy.reis@.com>
2025-01-11 17:08:51 -0300

    Tab

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ app = FastAPI()
 
 @app.get("/jobs/user")
 def get_user_cron_jobs(user: str):
-    result = api_crontab_get(user)
-    if not result:
-        error_msg = f"Erro ao ler o crontab do usuário {user}: {result.stderr}"
-        raise HTTPException(status_code=500, detail=error_msg)
-    return result
+	result = api_crontab_get(user)
+	if not result:
+		error_msg = f"Erro ao ler o crontab do usuário {user}: {result.stderr}"
+		raise HTTPException(status_code=500, detail=error_msg)
+	return result
 
 @app.get("/jobs/{user}/{id}")
 def get_cron_job(
-    user: str = Path(description="Linux user name"),
-    id: str = Path(description="cron job id")
+	user: str = Path(description="Linux user name"),
+	id: str = Path(description="cron job id")
 ):
-    result = api_cronjob_get_by_id(user, id)
-    if not result:
-        error_msg = f"Erro ao ler o crontab do usuário {user}: {result.stderr}"
-        raise HTTPException(status_code=500, detail=error_msg)
-    return result
+	result = api_cronjob_get_by_id(user, id)
+	if not result:
+		error_msg = f"Erro ao ler o crontab do usuário {user}: {result.stderr}"
+		raise HTTPException(status_code=500, detail=error_msg)
+	return result
 
 @app.post("/jobs/create-cron")
 async def create_cron_job(job: CronJob):
-    cjid = str(uuid.uuid4()).split("-")[0]
-    cron_line = f"\n#cjid:{cjid} % Created by cron-api\n{job.schedule} {job.command}\n"
-    success, message = add_cron_job(cron_line)
+	cjid = str(uuid.uuid4()).split("-")[0]
+	cron_line = f"\n#cjid:{cjid} % Created by cron-api\n{job.schedule} {job.command}\n"
+	success, message = add_cron_job(cron_line)
 
-    if not success:
-        raise HTTPException(status_code=500, detail=message)
-    
-    return {"message": message, "job": job.dict()}
+	if not success:
+		raise HTTPException(status_code=500, detail=message)
+	
+	return {"message": message, "job": job.dict()}
